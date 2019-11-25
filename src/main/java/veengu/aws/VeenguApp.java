7caf1132719d57067ccf4d2f68ba62603c966471
Evgeny Apenkin <eapenkin@gmail.com>
2019-11-25 09:50:22 +0400

    Added TODOs

--- src/main/java/veengu/aws/VeenguApp.java
+++ src/main/java/veengu/aws/VeenguApp.java
@@ -3,6 +3,12 @@ package veengu.aws;
 import software.amazon.awscdk.core.App;
 
 public class VeenguApp {
+
+    public static final int CONTAINER_PORT = 8080;
+    public static final String REGISTRY_NAME = "scala-sandbox";
+    public static final String REPOSITORY_NAME = "scala-sandbox";
+    public static final String BRANCH_NAME = "research/rest-api";
+
     public static void main(final String[] args) {
         App app = new App();
 
@@ -10,15 +16,17 @@ public class VeenguApp {
         // App Stacks
         ///////////////////////////////////////////////////////////////////////////
 
-        GitRepository gitRepository = new GitRepository(app, "GitRepository", "scala-sandbox");
-        DockerRegistry dockerRegistry = new DockerRegistry(app, "DockerRegistry", "scala-sandbox");
-        PublishPipeline publishPipeline = new PublishPipeline(app, "PublishPipeline", gitRepository.getRepository(), "research/rest-api", dockerRegistry.getRegistry());
+        GitRepository gitRepository = new GitRepository(app, "GitRepository", REPOSITORY_NAME);
+        DockerRegistry dockerRegistry = new DockerRegistry(app, "DockerRegistry", REGISTRY_NAME);
+        PublishPipeline publishPipeline = new PublishPipeline(app, "PublishPipeline", gitRepository.getRepository(), BRANCH_NAME, dockerRegistry.getRegistry());
         FargateCluster fargateCluster = new FargateCluster(app, "FargateCluster", dockerRegistry.getRegistry());
 
         ///////////////////////////////////////////////////////////////////////////
         // Stacks Dependencies
         ///////////////////////////////////////////////////////////////////////////
 
+        // publishPipeline.addDependency(gitRepository); TODO why it creates cyclic dependency?
+        publishPipeline.addDependency(dockerRegistry);
         fargateCluster.addDependency(publishPipeline);
 
         app.synth();
